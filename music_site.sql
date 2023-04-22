a78bb9417ed3af94e5c8040e50a44a84b3f2bfb5
Garry test <garry_79@mail.ru>
2023-04-22 12:28:08 +0300

    Initial commit

---
+++ music_site.sql
@@ -0,0 +1,46 @@
+CREATE TABLE IF NOT EXISTS Ganres (
+    ganre_id SERIAL PRIMARY KEY,
+	name_ganre VARCHAR(40) NOT NULL,
+);
+
+CREATE TABLE IF NOT EXISTS Performers (
+    performer_id SERIAL PRIMARY KEY,
+	name_performer VARCHAR(40) NOT NULL,
+);
+
+CREATE TABLE IF NOT EXISTS GenresPerformers (
+	ganre_id INTEGER REFERENCES Ganres(ganre_id),
+	performer_id INTEGER REFERENCES Performers(performer_id),
+	CONSTRAINT GenPer PRIMARY KEY (ganre_id, performer_id)
+);
+
+CREATE TABLE IF NOT EXISTS Albums (
+    album_id SERIAL PRIMARY KEY,
+	name_album VARCHAR(40) NOT NULL,
+    year_release_album INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS PerformersAlbums (
+	performer_id INTEGER REFERENCES Performers(performer_id),
+	album_id INTEGER REFERENCES Albums(album_id),
+	CONSTRAINT PerAlb PRIMARY KEY (performer_id, album_id)
+);
+
+CREATE TABLE IF NOT EXISTS Songs (
+    song_id SERIAL PRIMARY KEY,
+	name_song VARCHAR(40) NOT NULL,
+    duration_song TIME NOT NULL
+    album_id INTEGER NOT NULL REFERENCES Albums(album_id)
+);
+
+CREATE TABLE IF NOT EXISTS Collections (
+    collection_id SERIAL PRIMARY KEY,
+	name_collection VARCHAR(40) NOT NULL,
+    year_collection INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS CollectionsSongs (
+	song_id INTEGER REFERENCES Songs(song_id),
+	collection_id INTEGER REFERENCES Collections(collection_id),
+	CONSTRAINT ColSon PRIMARY KEY (song_id, collection_id)
+);
